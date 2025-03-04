b8a67e358f52a5a3e5b343d7d216d2543d570078
Warriormilan <warriormilan23@gmail.com>
2025-03-04 15:37:53 +0300

    2

--- AvaloniaApplication1/Window1.axaml.cs
+++ AvaloniaApplication1/Window1.axaml.cs
@@ -5,20 +5,39 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Security.Cryptography.X509Certificates;
+using Tmds.DBus.Protocol;
 using static AvaloniaApplication1.MainWindow;
 
 namespace AvaloniaApplication1;
 
 public partial class Window1 : Window
 {
+    ObservableCollection<listTovars> b;
     
     public Window1()
     {
         InitializeComponent();
+        nado.ItemsSource = null;
+        nado.ItemsSource = b;
     }
     public Window1(ObservableCollection<listTovars> a)
     {
+        b = a;
         InitializeComponent();
         nado.ItemsSource = a;
+        nado.SelectionChanged += listBox1_SelectedIndexChanged;
     }
+    private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (nado.SelectedItem is listTovars sel)
+        {
+            var lox123 = new Window2(sel,b);
+            lox123.Show();
+            Close();
+        }
+        
+        
+    }
+    
+    
 }
---
+++ AvaloniaApplication1/Window2.axaml.cs
@@ -0,0 +1,46 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using static AvaloniaApplication1.MainWindow;
+using System.Collections.ObjectModel;
+using System;
+
+namespace AvaloniaApplication1;
+
+public partial class Window2 : Window
+{
+    private listTovars WW;
+    private ObservableCollection<listTovars> ww;
+    public Window2()
+    {
+        InitializeComponent();
+    }
+    public Window2(listTovars a, ObservableCollection<listTovars> ww)
+    {
+        InitializeComponent();
+        WW = a;
+        this.ww = ww;
+        Name.Text = WW.name;
+        Price.Text = WW.price.ToString();
+        kolvo.Text = WW.kolichestvo.ToString();
+        Discription.Text = WW.discr;
+
+    }
+
+    private void Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        WW.name = Name.Text;
+        WW.price = Convert.ToDouble(Price.Text);
+        WW.discr = Discription.Text;
+        WW.kolichestvo = Convert.ToInt32(kolvo.Text);
+        new Window1(ww).Show();
+        Close();
+        
+    }
+
+    private void Button_Click_1(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        Close();
+    }
+    
+}
